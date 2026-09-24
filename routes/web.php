2dfa81478c662ea00a3deb30928a227f8f60f637
Ahmad Daneil <adwari132@gmail.com>
2026-09-24 15:04:11 +0800

    feat: implement order deletion functionality for incomplete orders and add related tests

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ use App\Http\Controllers\Staff\StaffDepositPaymentController;
 use App\Http\Controllers\Staff\StaffDesignWorkflowController;
 use App\Http\Controllers\Staff\StaffJobAssignmentController;
 use App\Http\Controllers\Staff\StaffOrderController;
+use App\Http\Controllers\Staff\StaffOrderDeletionController;
 use App\Http\Controllers\Staff\StaffOrderProductionAssignmentController;
 use App\Http\Controllers\Staff\StaffPackingWorkflowController;
 use App\Http\Controllers\Staff\StaffPrintingWorkflowController;
@@ -81,6 +82,8 @@ Route::middleware(['auth:admin', 'active.staff', 'staff.role:ADMIN'])
 
         Route::get('/orders', [StaffOrderController::class, 'index'])->name('orders.index');
         Route::get('/orders/{orderId}', [StaffOrderController::class, 'show'])->name('orders.show');
+        Route::delete('/orders/{order}', [StaffOrderDeletionController::class, 'destroy'])
+            ->name('orders.destroy');
         Route::post('/orders/{order}/assign-printing', [StaffOrderProductionAssignmentController::class, 'assignPrinting'])
             ->name('orders.assign-printing');
         Route::post('/orders/{order}/assign-packing-fulfilment', [StaffOrderProductionAssignmentController::class, 'assignPackingAndFulfilment'])
@@ -225,6 +228,8 @@ Route::middleware(['auth:staff', 'active.staff'])
         */
 
         Route::middleware('staff.role:ADMIN,'.User::ROLE_OM)->group(function () {
+            Route::delete('/orders/{order}', [StaffOrderDeletionController::class, 'destroy'])
+                ->name('orders.destroy');
             Route::post('/orders/{order}/assign-printing', [StaffOrderProductionAssignmentController::class, 'assignPrinting'])
                 ->name('orders.assign-printing');
             Route::post('/orders/{order}/assign-packing-fulfilment', [StaffOrderProductionAssignmentController::class, 'assignPackingAndFulfilment'])
